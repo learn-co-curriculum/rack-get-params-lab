742f280355d703217641326e01432ee16db52234
Joe Burgess <joe@flatironschool.com>
2015-11-28 13:44:30 -0500

    starter code and readme

---
+++ app/application.rb
@@ -0,0 +1,30 @@
+class Application
+
+  @@items = ["Apples","Carrots","Pears"]
+
+  def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
+
+    if req.path.match(/items/)
+      @@items.each do |item|
+        resp.write "#{item}\n"
+      end
+    elsif req.path.match(/search/)
+      search_term = req.params["q"]
+      resp.write handle_search(search_term)
+    else
+      resp.write "Path Not Found"
+    end
+
+    resp.finish
+  end
+
+  def handle_search(search_term)
+    if @@items.include?(search_term)
+      return "#{search_term} is one of our items"
+    else
+      return "Couldn't find #{search_term}"
+    end
+  end
+end
